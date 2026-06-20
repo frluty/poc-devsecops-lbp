ee925300409ced0b0768a5e2c942451e17138049
frluty <francoisluty@gmail.com>
2026-06-20 21:19:55 +0200

    test

--- config.py
+++ config.py
@@ -2,5 +2,4 @@
 AWS_ACCESS_KEY_ID = "AKIAI44QH8DHBEXAMPLE"
 AWS_SECRET_ACCESS_KEY = "je7MtGbClwBF/2Zp9Utk/h3yCo8nvbEXAMPLEKEY"
 GITHUB_TOKEN = "ghp_R2D2C3P0T800XWing12345abcdefghijklmn"
-GITHUB_TOKENd = "ghp_R2D2C3P0T800XWing12345abcdefghijklmn"
 DATABASE_URL = "postgresql://admin:S3cur3P@ssw0rd!@prod-db.banque.fr:5432/payments"
